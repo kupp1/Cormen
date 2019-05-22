2b9ee74713479364ea39c43819bca1c2c1fc8f9e
kupp <dobr1bobr47@gmail.com>
2019-05-23 00:38:42 +0300

    pcre decoration and code style fixes

--- Cormen.c
+++ Cormen.c
@@ -11,8 +11,8 @@
 #include <pcre.h>
 #include <locale.h>
 
-char *server = "irc.run.net";
-int port = 6660;
+char *server = "irc.freenode.net";
+int port = 6667;
 
 char *nick = "Cormen";
 char *username = "bot";
@@ -27,23 +27,22 @@ typedef struct
     char *regex;
     pcre *reCompiled;
     pcre_extra *pcreExtra;
-    int subStrVec[30];
-} prceRegex;
+    char *str;
+    int subStrInxs[30];
+    char* subStrs[15];
+} pcreRegex;
 
-prceRegex makeRegex(char *regex, const unsigned char *pcreTables);
-int execRegex(prceRegex *regex, char *str);
+pcreRegex makeRegex(char *regex, const unsigned char *pcreTables);
+int execRegex(pcreRegex *regex);
+int getGroups(pcreRegex *regex, int pcreExecRet);
+int doRegex(pcreRegex *regex, char* str);
 
 int main(int argc, char *argv[]) 
 {
-    int pcreExecRet = 0;
-    //int subStrVec[30];
-    //const char *pcreErrorStr;
-    //int pcreErrorOffset;
-    const char *psubStrMatchStr;
-    const unsigned char *pcreTables = NULL;
-         
     setlocale (LC_CTYPE, (const char *) "ru.");
-    pcreTables = pcre_maketables();
+    const unsigned char *pcreTables = pcre_maketables();
+
+    enum {VAR, IDENT, NICK, USERNAME, HOST, VERB, PARAMS, MSG};
     char *RFC2812 =
         "^" // We'll match the whole line. Start.
             // Optional prefix and the space that separates it
@@ -73,8 +72,8 @@ int main(int argc, char *argv[])
         ")"                   // captured in one reference
         "(?: :?(.*))"    // the rest, does not capture colon.
         "$"; // EOL
-    prceRegex re = makeRegex(RFC2812, pcreTables);
-    prceRegex *reP = &re;
+    pcreRegex re = makeRegex(RFC2812, pcreTables);
+    pcreRegex *reP = &re;
 
     /* char *str = "PING :irc.run.net\r\n";
     pcreExecRet = pcre_exec(re, pcreExtra, str, strlen(str), 0, 0, subStrVec, 30);
@@ -88,22 +87,30 @@ int main(int argc, char *argv[])
             printf("Match(%2d/%2d): (%2d,%2d): '%s'\n", j, pcreExecRet-1, subStrVec[j*2], subStrVec[j*2+1], psubStrMatchStr);
         }
     } */
-    int sockfd = 0, n = 0;
+    /* char *str = "PING :irc.run.net\r\n";
+    pcreExecRet = execRegex(reP, str);
+    getGroups(reP, str, pcreExecRet);
+    puts(reP->subStrs[5]); */
+    /* char *str1 = ":TomFarr!~tomfarr@top.secret.ip.there PRIVMSG #16bit :всем ква kva\r\n";
+    //pcreExecRet = execRegex(reP, str1);
+    int g = doRegex(reP, str1);
+    for(int i = 0; i < g; i++) {puts(reP->subStrs[i]);}
+    return 0; */
     char recvBuff[512];
     char sendBuff[512];
     struct sockaddr_in serv_addr;
-    struct hostent *host;
 
-    memset(recvBuff, '\0', sizeof(recvBuff));
-    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    //memset(recvBuff, '\0', sizeof(recvBuff));
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
     {
-        printf("\n Error : Could not create socket \n");
+        puts("Error : Could not create socket");
         return 1;
     }
-
-    if ( (host = gethostbyname(server)) == NULL )
+    struct hostent *host = gethostbyname(server);
+    if (host == NULL)
     {
-        printf("\n Invalid host\n");
+        puts("Invalid host");
         return 1;
     }
 
@@ -111,7 +118,7 @@ int main(int argc, char *argv[])
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(port);
 
-    if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
        printf("\n Error : Connect Failed \n");
        return 1;
@@ -121,31 +128,30 @@ int main(int argc, char *argv[])
     sendFromBuff(sockfd, sendBuff);
     sprintf(sendBuff, "USER %s 0 * :%s", username, realname);
     sendFromBuff(sockfd, sendBuff);
-    join(sockfd, sendBuff, "#16bits");
-    //join(sockfd, sendBuff, "#16bit");
-    while ( (n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0)
+    join(sockfd, sendBuff, "###kupp_trash");
+    int n;
+    while ( (n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0 )
     {
+
         recvBuff[n] = 0;
-        if(fputs(recvBuff, stdout) == EOF)
+        if (fputs(recvBuff, stdout) == EOF)
         {
             printf("\n Error : Fputs error\n");
         }
-        pcreExecRet = execRegex(reP, recvBuff);
-        if (pcreExecRet > 0)
+        int pcreExecRet = doRegex(reP, recvBuff);
+        if (pcreExecRet > 7)
         {
-            pcre_get_substring(recvBuff, reP->subStrVec, pcreExecRet, 5, &(psubStrMatchStr));
-            if (strcmp("PING", psubStrMatchStr) == 0)
+            if (strcmp("PING", reP->subStrs[VERB]) == 0)
             {
-                pcre_get_substring(recvBuff, reP->subStrVec, pcreExecRet, 7, &(psubStrMatchStr));
-                sprintf(sendBuff, "PONG %s", psubStrMatchStr);
+                sprintf(sendBuff, "PONG %s", reP->subStrs[MSG]);
                 sendFromBuff(sockfd, sendBuff);
             }
         }
     }
 
-    if(n < 0)
+    if (n < 0)
     {
-        printf("\n Read error \n");
+        puts("Read error");
     }
 
     return 0;
@@ -178,31 +184,52 @@ int join(int sockfd, char *Buff, char *channel)
     return 0;
 }
 
-prceRegex makeRegex(char *regex, const unsigned char *pcreTables)
+pcreRegex makeRegex(char *regex, const unsigned char *pcreTables)
 {
     const char *pcreErrorStr;
     int pcreErrorOffset;
     pcre *tmpReCompiled;
     pcre_extra *tmpPcreExtra;
     tmpReCompiled = pcre_compile(regex, 0, &pcreErrorStr, &pcreErrorOffset, pcreTables);
-    if(tmpReCompiled == NULL) 
+    if (tmpReCompiled == NULL) 
     {
         printf("ERROR: Could not compile regex '%s': %s\n", regex, pcreErrorStr);
         pcre_free(tmpReCompiled);
-        return (prceRegex){};
+        return (pcreRegex){};
     }
     tmpPcreExtra = pcre_study(tmpReCompiled, 0, &pcreErrorStr);
-    if(pcreErrorStr != NULL) 
+    if (pcreErrorStr != NULL) 
     {
         printf("ERROR: Could not study regex '%s': %s\n", regex, pcreErrorStr);
         pcre_free(tmpReCompiled);
         pcre_free_study(tmpPcreExtra);
-        return (prceRegex){};
+        return (pcreRegex){};
     }
-    return (prceRegex){.regex = regex, .reCompiled = tmpReCompiled, .pcreExtra = tmpPcreExtra};
+    return (pcreRegex){.regex = regex, .reCompiled = tmpReCompiled, .pcreExtra = tmpPcreExtra};
 }
 
-int execRegex(prceRegex *regex, char *str)
+int execRegex(pcreRegex *regex)
 {
-    return pcre_exec(regex->reCompiled, regex->pcreExtra, str, strlen(str), 0, 0, regex->subStrVec, 30);
+    return pcre_exec(regex->reCompiled, regex->pcreExtra, regex->str, strlen(regex->str), 0, 0, regex->subStrInxs, 30);
+}
+
+int getGroups(pcreRegex *regex, int pcreExecRet)
+{
+    const char *psubStrMatchStr;
+    for(int i = 0; i < pcreExecRet; i++)
+    {
+        pcre_get_substring(regex->str, regex->subStrInxs, pcreExecRet, i, &psubStrMatchStr);
+        regex->subStrs[i] = realloc(regex->subStrs[i], strlen(psubStrMatchStr) * sizeof(char) + 1);
+        strcpy(regex->subStrs[i], psubStrMatchStr);
+    }
+    return 0;
+}
+
+int doRegex(pcreRegex *regex, char* str)
+{
+    regex->str = str;
+    int ret = execRegex(regex);
+    if (ret)
+        getGroups(regex, ret);
+    return ret;
 }
